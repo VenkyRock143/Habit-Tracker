d20cfcf5bcac70f89fe292de8077dbdf88cd84b0
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2023-07-23 23:34:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./config/mongoose');
 const bodyParser = require('body-parser');
 
+//Middleware to take care of req,res of url that are encoded.
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(expressLayouts);
 
+//making assets folder as default static folder for express
 app.use(express.static('assets'));
 
 //connecting mongo DB
