e219e9944ca9abe89b28d10f996ec10c2bd3fe2b
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2023-07-24 16:47:54 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const connectDB = async() =>{
     try {
         const mongoosedb = process.env.MONGODB_URI || 'mongodb://localhost/habit_tracker';
-        await mongoose.connect(mongoosedb,{
+        const options = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        };
+        await mongoose.connect(mongoosedb,options);
         console.log("Mongo DB Connected!")
     } catch (error) {
         console.error('MongoDB connection error', error);
