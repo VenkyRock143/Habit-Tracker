b88b60c2788595744b82507f893082480d1b275b
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2023-07-24 18:52:27 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async() =>{
     try {
-        const mongoosedb =mongosh "mongodb+srv://habit.qxvyvn6.mongodb.net/" --apiVersion 1 --username venkybalusani1
-        const options = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            family: 4,
-        };
-        await mongoose.connect(mongoosedb,options);
+        // const mongoosedb = 'mongodb://localhost/habit_tracker'
+        // const options = {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        // };
+        // await mongoose.connect(mongoosedb,options);
+         await mongoose.connect('mongodb+srv://venkybalusani1:Rock4027@habit.z05oj4w.mongodb.net/?retryWrites=true&w=majority');
         console.log("Mongo DB Connected!")
     } catch (error) {
         console.error('MongoDB connection error', error);
