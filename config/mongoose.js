203ab5a2b15ffb13d4b7e4a09c902b40f7048b76
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2023-07-24 16:56:21 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,31 @@
-// const { Console } = require('console');
+// // const { Console } = require('console');
+// const mongoose = require('mongoose');
+
+// const connectDB = async() =>{
+//     try {
+//         const mongoosedb = process.env.MONGODB_URI || 'mongodb://localhost/habit_tracker';
+//         const options = {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         };
+//         await mongoose.connect(mongoosedb,options);
+//         console.log("Mongo DB Connected!")
+//     } catch (error) {
+//         console.error('MongoDB connection error', error);
+//     }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
+// connecting to mongoose
+mongoose.connect('mongodb://localhost/habit_tracker_development');
+// mongoose database connection
+const connectDB = mongoose.connection;
+db.on('error',console.error.bind(console,"Error connecting to MongoDb"));
 
-const connectDB = async() =>{
-    try {
-        const mongoosedb = process.env.MONGODB_URI || 'mongodb://localhost/habit_tracker';
-        const options = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        };
-        await mongoose.connect(mongoosedb,options);
-        console.log("Mongo DB Connected!")
-    } catch (error) {
-        console.error('MongoDB connection error', error);
-    }
-};
+db.once('open',function(){
+    console.log('Connected to Database :: MondoDB');
+});
 
-module.exports = connectDB;
+module.exports=connectDB;
