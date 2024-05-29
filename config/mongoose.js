a0d5a90df87fd293f147f71085279d75411433df
VenkyRock143 <106467934+VenkyRock143@users.noreply.github.com>
2024-05-29 11:00:20 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async() =>{
     try {
-         await mongoose.connect(process.env.MONGODB_URI);
+         await mongoose.connect('mongodb+srv://venkybalusani1:Rock4027@habit.z05oj4w.mongodb.net/?retryWrites=true&w=majority');
         console.log("Mongo DB Connected!")
     } catch (error) {
         console.error('MongoDB connection error', error);
